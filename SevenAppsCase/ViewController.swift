ba13e3f58fe37dc540147c91e9cd62ef194358ec
Ali Görkem Aksöz <96394804+AGorkemAksoz@users.noreply.github.com>
2025-01-30 13:43:43 +0300

    Initial Commit

---
+++ SevenAppsCase/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SevenAppsCase
+//
+//  Created by Gorkem on 30.01.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
